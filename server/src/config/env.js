70fe44deb58338c78d3d2304725b2221f569f05f
ritiklonare123 <ritiklonare5@gmail.com>
2026-09-10 01:06:00 +0530

    feat: implement JWT access and refresh token authentication

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const env = {
   port : process.env.PORT || 3000,
   MONGODB_URI : process.env.MONGODB_URI,
-  JWT_SECRET : process.env.JWT_SECRET,
+  JWT_ACCESS_SECRET :process.env.JWT_ACCESS_SECRET,
+  JWT_REFRESH_SECRET :process.env.JWT_REFRESH_SECRET,      
 }
 
 module.exports = env;
